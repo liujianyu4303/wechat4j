198d23cf6c085e8a0c6a86dbd645c19a22f844f3
ljy <ljy>
2016-04-11 21:15:43 +0800

    aasdsad

---
+++ src/org/sword/wechat4j/message/TestController.java
@@ -0,0 +1,14 @@
+package org.sword.wechat4j.message;
+
+/**
+ * 类描述：jfinal demo 的例子
+ * <pre>
+ * -------------History------------------
+ *   DATE                 AUTHOR         VERSION        DESCRIPTION
+ *  2016年04月11日            刘建宇      V01.00.001		   新增内容
+ * </pre>
+ *
+ * @author <a href="mailto:liujy@dayanginfo.com">刘建宇</a>
+ */
+public class TestController {
+}
